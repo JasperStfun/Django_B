b08c952c2def10b8a23e3144e1c13e92be26c473
JasperStFun <kosto55@mail.ru>
2021-10-22 15:12:05 +0300

    fin

--- myfirst/myfirst/apps/articles/views.py
+++ myfirst/myfirst/apps/articles/views.py
@@ -1,5 +1,34 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+from .models import Article
 
 
 def index(request):
-    return HttpResponse('Hello World')
+    latest_articles = Article.objects.order_by('-pub_date')[:5]
+    return render(request, 'articles/list.html', {'latest_articles': latest_articles})
+
+
+def detail(request, article_id):
+    try:
+        a = Article.objects.get(id=article_id)
+    except:
+        raise Http404('Article not found')
+    
+    latests_coments = a.comment_set.order_by('-id')[:10]
+    
+    return render(request, 'articles/detail.html', {'article': a, 
+                                                    'latests_comments': latests_coments})
+
+
+def leave_comment(request, article_id):
+    try:
+        a = Article.objects.get(id=article_id)
+    except:
+        raise Http404('Article not found')
+    
+    a.comment_set.create(author_name = request.POST['name'], 
+                         comment_text=request.POST['text'])
+    
+    return HttpResponseRedirect(reverse('articles:detail', args=(a.id,)))
--- myfirst/myfirst/settings.py
+++ myfirst/myfirst/settings.py
@@ -15,10 +15,10 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-PROJECT_DIR = Path(__file__).resolve().parent / 'apps'
+PROJECT_ROOT = Path(__file__).resolve().parent / 'apps'
 
 
-sys.path.append(str(PROJECT_DIR))
+sys.path.append(str(PROJECT_ROOT))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -60,7 +60,9 @@ ROOT_URLCONF = 'myfirst.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(PROJECT_ROOT, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,7 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
